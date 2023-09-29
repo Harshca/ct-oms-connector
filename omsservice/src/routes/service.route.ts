67173dbc427994cc9dafbc209f498ddbbe8f51c9
Harshvardhan Sharma <harsharma@deloitte.ca>
2023-09-29 10:22:48 -0400

    changed order state

--- omsservice/src/routes/service.route.ts
+++ omsservice/src/routes/service.route.ts
@@ -7,10 +7,11 @@ const serviceRouter = Router();
 serviceRouter.post('/submitOrder', async (req, res) => {
   logger.info('Order submit extension executed');
   logger.info(JSON.stringify(req.body.resource.obj.id));
-  // const response = await submitOrder(req.body.resource.obj.id);
-  // res.status(response?.statusCode || 400);
-  submitOrder(req.body.resource.obj.id);
-  res.sendStatus(200);
+  const response = await submitOrder(req.body.resource.obj.id);
+  res.status(response?.statusCode || 400).send({
+    "action": "changeOrderState",
+    "orderState": "Confirmed"
+  });
 });
 
 export default serviceRouter;
